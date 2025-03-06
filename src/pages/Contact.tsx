d61ff2e4688d4ba200b60225b6f6342326382af2
MahdiHDev <mahdi.hussain.work@gmail.com>
2025-03-07 01:38:18 +0600

    contact page desktop view complete

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,6 @@
-import { CiLocationOn } from 'react-icons/ci';
-import { FiPhoneCall } from 'react-icons/fi';
-import { TfiEmail } from 'react-icons/tfi';
+import ContactIcon from '../assets/images/contact/ContactIcon';
+import Mail from '../assets/images/contact/Mail';
+import MapPin from '../assets/images/contact/MapPin';
 import Breadcrumbs from '../components/Breadcrumbs';
 
 const Contact = () => {
@@ -8,41 +8,68 @@ const Contact = () => {
         <div>
             <Breadcrumbs />
             <div className="max-width">
-                <div>
-                    <div>
-                        <CiLocationOn />
-                        <p>2715 Ash Dr. San Jose, South Dakota 83475</p>
+                <div className="flex py-20 gap-6">
+                    <div className="w-1/4 shadow-box">
+                        <div className="flex flex-col items-center justify-center px-5 py-6 text-center">
+                            <MapPin className="text-[#2C742F] size-13" />
+                            <p className="text-[#333333] leading-6 py-4">
+                                2715 Ash Dr. San Jose, South Dakota 83475
+                            </p>
+                        </div>
+                        <div className="flex flex-col items-center justify-center px-5 py-6 text-center">
+                            <Mail className="text-[#2C742F] size-13" />
+                            <div className="leding-6 py-4 text-[#333333]">
+                                <p>Proxy@gmail.com</p>
+                                <p> Help.proxy@gmail.com</p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col items-center justify-center px-5 py-6 text-center">
+                            <ContactIcon className="text-[#2C742F] size-13" />
+                            <div className="leding-6 py-4 text-[#333333]">
+                                <p>(219) 555-0114</p>
+                                <p>(164) 333-0487</p>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <TfiEmail />
-                        <p>Proxy@gmail.com</p>
-                        <p> Help.proxy@gmail.com</p>
-                    </div>
-                    <div>
-                        <FiPhoneCall />
-                        <p>(219) 555-0114</p>
-                        <p>(164) 333-0487</p>
-                    </div>
-                </div>
 
-                <div>
-                    <h3>Just Say Hello!</h3>
-                    <p>
-                        Do you fancy saying hi to me or you want to get started
-                        with your project and you need my help? Feel free to
-                        contact me.
-                    </p>
-                    <form>
-                        <input type="text" placeholder="Template Cookie" />
-                        <input type="text" placeholder="zakirsoft@gmail.com" />
-                        <input type="text" placeholder="Text here" />
-                        <textarea
-                            name=""
-                            id=""
-                            placeholder="Subjects"
-                        ></textarea>
-                    </form>
-                    <button>Send Message</button>
+                    <div className="w-3/4 shadow-box p-10">
+                        <h3 className="text-[#1A1A1A] font-semibold text-2xl pt-1">
+                            Just Say Hello!
+                        </h3>
+                        <p className="text-[#808080] text-sm w-[55%] pt-2">
+                            Do you fancy saying hi to me or you want to get
+                            started with your project and you need my help? Feel
+                            free to contact me.
+                        </p>
+                        <form className="py-6 space-y-4">
+                            <div className="flex gap-2">
+                                <input
+                                    type="text"
+                                    placeholder="Template Cookie"
+                                    className="border border-[#E6E6E6] w-1/2 outline-primary py-3 text-sm px-4 rounded-md text-[#1a1a1a]"
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="zakirsoft@gmail.com"
+                                    className="border border-[#E6E6E6] w-1/2 outline-primary py-3 text-sm px-4 rounded-md text-[#1a1a1a]"
+                                />
+                            </div>
+                            <input
+                                type="text"
+                                placeholder="Text here"
+                                className="border border-[#E6E6E6] w-full outline-primary py-3 text-sm px-4 rounded-md text-[#1a1a1a]"
+                            />
+                            <textarea
+                                name=""
+                                id=""
+                                placeholder="Subjects"
+                                className="border border-[#E6E6E6] w-full outline-primary py-3 text-sm px-4 rounded-md text-[#1a1a1a] h-[100px]"
+                            ></textarea>
+                        </form>
+                        <button className="py-4 px-10 rounded-[43px] cursor-pointer bg-primary text-white">
+                            Send Message
+                        </button>
+                    </div>
                 </div>
             </div>
             <div className="w-full">
